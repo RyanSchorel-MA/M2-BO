eeea35b2971abe9d3995dcd74aa30d75cdfb4d04
Ryan Schorel <34758@ma-web.nl>
2023-01-26 11:50:46 +0100

    bezig met taal

---
+++ HTML M2-BO/Canvas/gamelogic.js
@@ -0,0 +1,43 @@
+import { overlap } from "./collisionmath.js";
+import { Rect } from "./rect.js";
+export class Gamelogic{
+
+    constructor(game){
+        this.game = game;
+
+
+    
+    
+
+    }
+
+    mouseMoved(event){
+
+        this.game.player.x = event.offsetX;
+        this.game.player.y = event.offsetY;
+
+    }
+
+    mousedown(event){
+       
+        this.game.playerSprite = 1;
+     
+    }
+
+    mouseup(event){
+
+        this.game.playerSprite = 0;
+       }
+
+  
+
+    logic(){
+
+        if(overlap(this.game.player, this.game.mole) == true && this.game.playerSprite == 1){
+
+            this.game.moleState = 3;
+            
+        }
+
+    }
+}
